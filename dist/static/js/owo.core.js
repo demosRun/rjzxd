2bc5669e4b4b6bb5bde2b6fa13e7e4cf94758501
puge <mail@puge.org>
2020-12-10 15:44:06 +0800

    save

--- dist/static/js/owo.core.js
+++ dist/static/js/owo.core.js
@@ -1,4 +1,4 @@
-// Wed Dec 09 2020 17:14:34 GMT+0800 (GMT+08:00)
+// Thu Dec 10 2020 15:43:46 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {
@@ -410,6 +410,14 @@ _owo.animation = function (oldDom, newDom, animationIn, animationOut, forward) {
 
 
 
+
+// 计算$dom
+var idList = document.querySelectorAll('[id]')
+owo.id = {}
+for (var ind = 0; ind < idList.length; ind++) {
+  var item = idList[ind]
+  owo.id[item.getAttribute('id')] = item
+}
 
 // 判断是否为手机
 _owo.isMobi = navigator.userAgent.toLowerCase().match(/(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null
